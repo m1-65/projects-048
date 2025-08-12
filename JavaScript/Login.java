05e0f80530ba84924c83fc462e00e2a14c1145ec
m1-65 <met61078@gmail.com>
2025-08-12 08:04:29 +0700

    354643131

--- JavaScript/Login.java
+++ JavaScript/Login.java
@@ -3,7 +3,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
   event.preventDefault();
 
   // กำหนดชื่อผู้ใช้และรหัสผ่านที่ถูกต้อง
-  const correctUsername = "user";
+  const correctUsername = "met";
   const correctPassword = "123";
 
   // ดึงค่าที่ผู้ใช้กรอกจากฟอร์ม
@@ -14,7 +14,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
   // ตรวจสอบข้อมูล
   if (username === correctUsername && password === correctPassword) {
     // หากข้อมูลถูกต้อง ให้เปลี่ยนหน้าไปที่ success.html
-    window.location.href = "success.html";
+    window.location.href = "home.html";
   } else {
     // หากข้อมูลไม่ถูกต้อง ให้แสดงข้อความแจ้งเตือนสีแดง
     errorMessage.textContent = "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง";
